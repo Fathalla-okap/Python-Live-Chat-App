5b530ea0e10060ee0511631ffc7cbeb59da88c24
Fathalla-okap <136493772+Fathalla-okap@users.noreply.github.com>
2024-06-24 22:42:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, session
-from flask_socketio import send, emit, SocketIO
+from flask import Flask, render_template, request, session, redirect, url_for, jsonify
+from flask_socketio import join_room, leave_room, send, SocketIO
+import random
+from string import ascii_uppercase
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "hjhjsdahhds"
@@ -7,21 +9,19 @@ socketio = SocketIO(app)
 
 messages = []
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["POST", "GET"])
 def home():
-    session.clear()
-    session["name"] = "unknown"
     return render_template("room.html", messages=messages)
 
 @socketio.on("message")
 def message(data):
     content = {
-        "name": session.get("name"),
+        "name": session.get("name", "unknown"),
         "message": data["data"]
     }
     messages.append(content)
     send(content, broadcast=True)
-    print(f"{session.get('name')} said: {data['data']}")
+    print(f"{content['name']} said: {content['message']}")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
